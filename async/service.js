3fd7596ebaa382cae5cb444e2144276f81d194d7
Tyler Graf <tylergraf@gmail.com>
2017-10-07 01:47:04 -0600

    async await service

--- async/service.js
+++ async/service.js
@@ -7,18 +7,19 @@ function getFromCache(url){
     }
   });
 }
-function getData(url) {
-  return getFromCache(url) // returns a promise
-    .catch(e => {
-      return fetch(url).then(res=>res.json()); // returns a promise
-    })
-    .then(v => {
-      sessionStorage[url] = JSON.stringify(v); // set cache
-      return Promise.resolve(v); // returns a promise
-    });
+async function getData(url) {
+  let data;
+  try {
+    data = await getFromCache(url) // returns a promise
+  } catch(e) {
+    data = await fetch(url).then(res=>res.json()); // returns a promise
+  }
+
+  sessionStorage[url] = JSON.stringify(data); // set cache
+  return Promise.resolve(data); // returns a promise
 }
 
 getData('./data.json')
   .then(data=>{
-
+    console.log(data);
   });
